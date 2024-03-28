8917f50fc51c388043ba2b5d65544f1f1038ab0a
wochap <gean.marroquin@gmail.com>
2024-03-28 09:49:20 -0500

    update flake to support dwl build with scenefx

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
-  description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=317484b1ead87b9c1b8ac5261a8d2dd748a0492d"; # nixos-unstable (jan 10 2024)
+  inputs.nixpkgs.url =
+    "github:nixos/nixpkgs?rev=b06025f1533a1e07b6db3e75151caa155d1c7eb3"; # nixos-unstable (mar 21 2024)
   inputs.nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
   inputs.nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
@@ -8,39 +8,71 @@
   outputs = { self, nixpkgs, nixpkgs-wayland, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
         pkgs-wayland = nixpkgs-wayland.packages.${system};
-        wlroots_0_16 = pkgs.wlroots_0_16;
-        wlroots_0_17 = pkgs.wlroots_0_17;
         wlroots_0_17_main = pkgs-wayland.wlroots;
-        packages = with pkgs; [
-          installShellFiles
-          pkg-config
-          wayland-scanner
-
-          bashInteractive
-
-          libinput
-          xorg.libxcb
-          libxkbcommon
-          pixman
-          wayland
-          wayland-protocols
-
-          xorg.libX11
-          xorg.xcbutilwm
-          xwayland
-
-          bear # to generate compile_commands.json
-        ];
+        pkgs = nixpkgs.legacyPackages.${system};
+        wlroots_0_17 = pkgs.wlroots_0_17;
+        scenefx = pkgs.callPackage ./scenefx.nix { };
       in {
         devShells.default = pkgs.mkShell {
-          packages = packages ++ [ wlroots_0_17 ];
+          packages = with pkgs; [
+            bear # to generate compile_commands.json
+            wlroots_0_17
+
+            # dwl nativeBuildInputs
+            installShellFiles
+            pkg-config
+            wayland-scanner
+
+            # dwl buildInputs
+            libinput
+            libxcb
+            libxkbcommon
+            pixman
+            wayland
+            wayland-protocols
+            libX11
+            xcbutilwm
+            xwayland
+          ];
 
           WAYLAND_SCANNER = "wayland-scanner";
         };
-        devShells.wlroots_0_16 = pkgs.mkShell {
-          packages = packages ++ [ wlroots_0_16 ];
+        devShells.scenefx = pkgs.mkShell {
+          packages = with pkgs; [
+            bear # to generate compile_commands.json
+            scenefx
+
+            # wlroots nativeBuildInputs
+            meson
+            ninja
+            pkg-config
+            wayland-scanner
+            glslang
+
+            # wlroots buildInputs
+            libGL
+            libcap
+            libinput
+            libpng
+            libxkbcommon
+            mesa
+            pixman
+            seatd
+            vulkan-loader
+            wayland
+            wayland-protocols
+            xorg.libX11
+            xorg.xcbutilerrors
+            xorg.xcbutilimage
+            xorg.xcbutilrenderutil
+            xorg.xcbutilwm
+            xwayland
+            ffmpeg
+            hwdata
+            libliftoff
+            libdisplay-info
+          ];
 
           WAYLAND_SCANNER = "wayland-scanner";
         };
