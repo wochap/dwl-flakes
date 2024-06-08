bff0403f78f243a16b90dfa136b771bcb62be95a
wochap <gean.marroquin@gmail.com>
2024-06-08 09:50:52 -0500

    refactor

--- flake.nix
+++ flake.nix
@@ -2,22 +2,18 @@
   inputs = {
     nixpkgs.url =
       "github:nixos/nixpkgs?rev=b06025f1533a1e07b6db3e75151caa155d1c7eb3"; # nixos-unstable (mar 21 2024)
-    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
-    nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     scenefx.url =
       "github:wlrfx/scenefx?rev=2ec3505248e819191c37cb831197629f373326fb";
     scenefx.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-wayland, flake-utils, hyprland, scenefx }:
+  outputs = { self, nixpkgs, flake-utils, scenefx }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs-wayland = nixpkgs-wayland.packages.${system};
-        wlroots_0_17_main = pkgs-wayland.wlroots;
         pkgs = nixpkgs.legacyPackages.${system};
         wlroots_0_17 = pkgs.wlroots_0_17;
-        scenefxPkg = pkgs.callPackage ./scenefx.nix { };
+        scenefx = scenefx.packages."${system}".scenefx;
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
@@ -46,45 +42,8 @@
         devShells.scenefx = pkgs.mkShell {
           packages = with pkgs; [
             bear # to generate compile_commands.json
-            scenefxPkg
 
-            # wlroots nativeBuildInputs
-            meson
-            ninja
-            pkg-config
-            wayland-scanner
-            glslang
-
-            # wlroots buildInputs
-            libGL
-            libcap
-            libinput
-            libpng
-            libxkbcommon
-            mesa
-            pixman
-            seatd
-            vulkan-loader
-            wayland
-            wayland-protocols
-            xorg.libX11
-            xorg.xcbutilerrors
-            xorg.xcbutilimage
-            xorg.xcbutilrenderutil
-            xorg.xcbutilwm
-            xwayland
-            ffmpeg
-            hwdata
-            libliftoff
-            libdisplay-info
-          ];
-
-          WAYLAND_SCANNER = "wayland-scanner";
-        };
-        devShells.scenefx-flake = pkgs.mkShell {
-          packages = with pkgs; [
-            bear # to generate compile_commands.json
-            scenefx.packages."${system}".scenefx
+            scenefx
             libGL
 
             # nativeBuildInputs
@@ -106,45 +65,6 @@
           ];
 
           WAYLAND_SCANNER = "wayland-scanner";
-        };
-        devShells.foot = pkgs.mkShell {
-          packages = with pkgs; [
-            wayland-scanner
-            meson
-            ninja
-            ncurses
-            scdoc
-            pkg-config
-
-            tllist
-            wayland-protocols
-            fontconfig
-            freetype
-            pixman
-            wayland
-            libxkbcommon
-            fcft
-            utf8proc
-          ];
-
-          CFLAGS = "-O3";
-
-        };
-        devShells.sfwbar = pkgs.mkShell {
-          packages = with pkgs; [
-            meson
-            ninja
-            pkg-config
-            makeWrapper
-
-            gtk3
-            json_c
-            gtk-layer-shell
-            libpulseaudio
-            libmpdclient
-            libxkbcommon
-            alsa-lib
-          ];
         };
       });
 }
