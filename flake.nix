73e8a74c0181f071e0dcec94f89f4261a6f180eb
wochap <gean.marroquin@gmail.com>
2024-08-02 10:37:37 -0500

    feat: upgrade to wlroots 0.18

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url =
-      "github:nixos/nixpkgs?rev=ad0b5eed1b6031efaed382844806550c3dcb4206"; # nixos-unstable (jul 19 2024)
+      "github:nixos/nixpkgs?rev=9f918d616c5321ad374ae6cb5ea89c9e04bf3e58"; # nixos-unstable (aug 02 2024)
     flake-utils.url = "github:numtide/flake-utils";
     scenefx.url =
       "github:wlrfx/scenefx?rev=7a7c35750239bd117931698b121a072f30bc1cbe";
@@ -13,12 +13,14 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         wlroots_0_17 = pkgs.wlroots_0_17;
+        wlroots_0_18 = pkgs.wlroots_0_18;
         scenefx-final = scenefx.packages."${system}".scenefx;
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             bear # to generate compile_commands.json
-            wlroots_0_17
+            # wlroots_0_17
+            wlroots_0_18
 
             # dwl nativeBuildInputs
             installShellFiles
@@ -42,6 +44,8 @@
         devShells.scenefx = pkgs.mkShell {
           packages = with pkgs; [
             bear # to generate compile_commands.json
+            # wlroots_0_17
+            wlroots_0_18
 
             scenefx-final
             libGL
@@ -58,7 +62,6 @@
             pixman
             wayland
             wayland-protocols
-            wlroots
             xorg.libX11
             xorg.xcbutilwm
             xwayland
