fa4956b6869eb8962a3103878736abcfbb66267c
wochap <gean.marroquin@gmail.com>
2024-08-11 12:58:56 -0500

    feat: add wlroots 0.19

--- flake.nix
+++ flake.nix
@@ -6,14 +6,18 @@
     scenefx.url =
       "github:wlrfx/scenefx?rev=7a7c35750239bd117931698b121a072f30bc1cbe";
     scenefx.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-wayland.url =
+      "github:nix-community/nixpkgs-wayland?rev=34d357829660a9f84e4f23ef06e461b9cca89212"; # master (aug 11 2024)
+    nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, scenefx }:
+  outputs = { self, nixpkgs, flake-utils, scenefx, nixpkgs-wayland }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         wlroots_0_17 = pkgs.wlroots_0_17;
         wlroots_0_18 = pkgs.wlroots_0_18;
+        wlroots_0_19 = nixpkgs-wayland.packages."${system}".wlroots;
         scenefx-final = scenefx.packages."${system}".scenefx;
       in {
         devShells.default = pkgs.mkShell {
@@ -21,6 +25,32 @@
             bear # to generate compile_commands.json
             # wlroots_0_17
             wlroots_0_18
+
+            # dwl nativeBuildInputs
+            installShellFiles
+            pkg-config
+            wayland-scanner
+
+            # dwl buildInputs
+            libinput
+            xorg.libxcb
+            libxkbcommon
+            pixman
+            wayland
+            wayland-protocols
+            xorg.libX11
+            xorg.xcbutilwm
+            xwayland
+          ];
+
+          WAYLAND_SCANNER = "wayland-scanner";
+        };
+        devShells.latest = pkgs.mkShell {
+          packages = with pkgs; [
+            bear # to generate compile_commands.json
+            # wlroots_0_17
+            # wlroots_0_18
+            wlroots_0_19
 
             # dwl nativeBuildInputs
             installShellFiles
