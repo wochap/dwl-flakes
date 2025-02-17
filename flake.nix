db4b5affb09c2b13a839fb5295489ed31ab621bc
wochap <gean.marroquin@gmail.com>
2025-02-17 07:06:02 -0500

    upgrade scenefx

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
       "github:nixos/nixpkgs?rev=b681065d0919f7eb5309a93cea2cfa84dec9aa88"; # nixos-24.11 (04 dec 2024)
     flake-utils.url = "github:numtide/flake-utils";
     scenefx.url =
-      "github:wlrfx/scenefx?rev=9aca3a3674f878fee0128240e2e24aeaabadea78"; # main (15 feb 2024)
+      "github:wlrfx/scenefx?rev=87c0e8b6d5c86557a800445e8e4c322f387fe19c"; # main (17 feb 2024)
     scenefx.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs-wayland.url =
       "github:nix-community/nixpkgs-wayland?rev=52b72b12c456a5c0c87c40941ef79335e8d61104"; # master (03 sep 2024)
